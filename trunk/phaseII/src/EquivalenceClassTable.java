d4a1e74285a9914b53adfbf3e4be34dbc3f0634e
jonkjetiloye <jonkjetiloye@9823c674-c500-6cfc-4aa4-e7b5df566407>
2011-09-17 10:19:23 +0000

    169 holecard rollout ok. Equivalence class table datastructure next

---
+++ trunk/phaseII/src/EquivalenceClassTable.java
@@ -0,0 +1,13 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: oye
+ * Date: 17.09.11
+ * Time: 11:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class EquivalenceClassTable {
+
+
+
+
+}
